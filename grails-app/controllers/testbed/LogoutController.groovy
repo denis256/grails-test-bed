fd6b74010c3613b92f70c0f6fc7045e6abb9989e
Denis O <denis@universal-development.com>
2015-12-27 00:44:14 +0200

    Basic logout controller

---
+++ grails-app/controllers/testbed/LogoutController.groovy
@@ -0,0 +1,12 @@
+package testbed
+
+class LogoutController {
+
+    /**
+     * Index action. Redirects to the Spring security logout uri.
+     */
+    def index = {
+        session.invalidate()
+        redirect (uri: '/')
+    }
+}
