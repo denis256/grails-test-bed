3e167a2780b39cb0bc97b6508eb190277f72e2dd
Denis O <denis@universal-development.com>
2015-12-28 17:17:23 +0200

    Authentification cleanup

--- grails-app/init/testbed/SecurityConfiguration.groovy
+++ grails-app/init/testbed/SecurityConfiguration.groovy
@@ -1,12 +1,11 @@
-package testbed;
+package testbed
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Configuration
-import org.springframework.core.annotation.Order;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 
 @Configuration
 @EnableWebSecurity
@@ -15,12 +14,13 @@ class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+
             .logout()
                 .logoutUrl("/logout")
                 .logoutSuccessUrl("/")
                 .and()
-                .antMatcher("/admin/*")
 
+                .antMatcher("/admin/*")
                 .authorizeRequests()
                 .anyRequest().authenticated()
 
